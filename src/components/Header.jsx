aa8967b798824741ddcd6296674fdcfd764bae9e
SatishK2022 <sk7399052@gmail.com>
2024-02-21 15:32:01 +0530

    Solve the Signup UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -47,11 +47,11 @@ function Header() {
               ))
             : null}
         </ul>
-        <div className="flex gap-4 text-black">
+        <div className="flex gap-4">
           <Button
             name="Sign Up"
             path={"/signup"}
-            className="text-black hover:bg-zinc-400/10"
+            className="text-black bg-white hover:bg-zinc-400/10"
           />
           <Button name="Log In" path={"/login"} />
         </div>
